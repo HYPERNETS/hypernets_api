d92c944639e332b35d311081b2127f07fa154995
Pieter De Vis <pieter.de.vis@npl.co.uk>
2026-06-04 16:12:32 +0100

    FIX DOCS?

--- examples/example_archive/example_query_download.py
+++ examples/example_archive/example_query_download.py
@@ -1,20 +1,21 @@
 from hypernets_api import HYPERNETSAPI
 
 api_token = "<your-api-token>" # generate on https://landhypernet.org.uk/api/token/generate
-api_token = "95atDYbhe4y9CzEtbsmeRilMl7b-XqnOUqCMr2P_FkI"
 
 output_path= "./"
 
 api = HYPERNETSAPI(api_token)
 
-query_dict = {"site":"GHNA",
-        "product_level":"L2A_REF",
-        "start_time": "2023-10-31T08:00:00Z",
-        "stop_time": "2023-11-01T08:00:00Z",
+
+query_dict = {
+    "site": "JSIT",
+    "product_level": "L2B",
+    "start_time": '2025-07-27',
+    "stop_time": '2025-07-28',
 }
 
 results = api.query(query_dict)
 print(results)
-#paths = api.download_results(results, output_path=output_path)
-
+paths = api.download_results(results, output_path=output_path)
+print(paths)
 
